14d996057142251a01f59cc030b368132882e30d
Brenno Garcia <brenno.garcia@e-deploy.com.br>
2021-11-09 03:55:31 -0300

    commit changes

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react'
 import Helmet from 'react-helmet'
-import MainScreen from 'app/screens/main'
+import MainScreen from 'screens/main'
 
 class App extends PureComponent {
 
