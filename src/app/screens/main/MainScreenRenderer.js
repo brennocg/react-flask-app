c514c282bf9c857e633911503a429ec3ae65ebc6
Brenno Garcia <brenno.garcia@e-deploy.com.br>
2021-11-09 04:45:55 -0300

    commit changes

--- src/app/screens/main/MainScreenRenderer.js
+++ src/app/screens/main/MainScreenRenderer.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
 
 export default class MainScreenRenderer extends Component {
   render() {
     return (
       <div >
-        <Helmet>
-          <title>Example Base</title>
-        </Helmet>
         <header >
 
           ... no changes in this part ...
