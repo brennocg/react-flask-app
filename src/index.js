3c7ccc2f7db58e20f83a8bc5319bbd6527b79c51
Brenno Garcia <brenno.garcia@e-deploy.com.br>
2021-11-09 03:24:38 -0300

    commit changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import '@formatjs/intl-pluralrules/polyfill'
-import '@formatjs/intl-pluralrules/locale-data/en'
-
 import React from 'react'
 import { render } from 'react-dom'
 import 'style/style.css'
