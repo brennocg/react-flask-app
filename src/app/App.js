1f28a290587bd37a1f9be7101c8ef3fe80bcfbf0
Brenno Garcia <brenno.garcia@e-deploy.com.br>
2021-11-09 02:40:52 -0300

    commit changes

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import 'App.css';
+import React, { PureComponent } from 'react'
+import 'App.css'
 
-function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+class App extends PureComponent {
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
+  render() {
+    return (
+      <div >
+        <header >
 
-  return (
-    <div className="App">
-      <header className="App-header">
+          ... no changes in this part ...
 
-        ... no changes in this part ...
-
-        <p>The current time is {currentTime}.</p>
-      </header>
-    </div>
-  );
+          <p>The current time is .</p>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
