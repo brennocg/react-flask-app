fcd30032da7ece35c1192612188e4eef4556b9e8
Brenno Garcia <brenno.garcia@e-deploy.com.br>
2021-11-09 02:44:14 -0300

    commit changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import 'style/style.css'
-import App from 'app/App'
+import App from 'app'
 
 render(
-    <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-      document.getElementById('root')
+    <App />
+    , document.getElementById('root')
 )
